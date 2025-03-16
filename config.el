6192f7e0ef287d10e56cdedcce7b4231bef31ea3
koprotk <dmunozm@outlook.cl>
2025-03-16 15:49:55 -0300

    Remove submodule add package to package.el

--- config.el
+++ config.el
@@ -292,3 +292,20 @@ If a method is provided, use -method instead of -testclass."
        :desc "Run TestNG test" "r" #'run-testng
        :desc "Debug TestNG test" "d" #'run-testng-debug
        :desc "Copy TestNG class" "c" #'copy-testng-classname))
+
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)
+              ("C-n" . 'copilot-next-completion)
+              ("C-p" . 'copilot-previous-completion))
+
+  :config
+  (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode 2))
+  (add-to-list 'copilot-indentation-alist '(text-mode 2))
+  (add-to-list 'copilot-indentation-alist '(closure-mode 2))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
--- packages.el
+++ packages.el
@@ -11,6 +11,10 @@
 (package! circadian)
 (package! csv-mode)
 (package! org-ref)
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :file ("*.el")))
+(package! lsp-latex
+  :recipe (:host github :repo "ROCKTAKEY/lsp-latex" :file ("lsp-latex.el")))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
